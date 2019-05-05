4843125549e5068ff9626a94b71e14c26b9e1284
Mike Wong <wongmdesign@gmail.com>
2019-05-05 10:50:08 -0400

    all new tables added

--- directors_table.sql
+++ directors_table.sql
@@ -29,3 +29,32 @@ FROM
 
 DROP TABLE actors;
 
+CREATE TABLE movies (
+  movie_id SERIAL PRIMARY KEY,
+  movie_name VARCHAR(50) NOT NULL,
+  movie_length INT,
+  release_date DATE,
+  age_certificate VARCHAR(5),
+  director_id INT REFERENCES directors (director_id) --foreign key reference
+
+)
+CREATE TABLE movie_revenues (
+  revenue_id SERIAL PRIMARY KEY,
+  movie_id INT REFERENCES movies (movie_id),
+  domestic_takings NUMERIC(6, 2),
+  international_takings NUMERIC(6, 2)
+);
+
+-- This is junction table with just 2 columns which are FKs
+-- Many to Many relationship
+
+CREATE TABLE movies_actors (
+  movie_id INT REFERENCES movies (movie_id),
+  actor_id INT REFERENCES actors (actor_id),
+  PRIMARY KEY (movie_id, actor_id)
+);
+
+SELECT
+  *
+FROM
+  movies_actors
