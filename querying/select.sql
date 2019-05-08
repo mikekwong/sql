cdd79a50830311781c41b7a71508044f7decdf33
Mike Wong <wongmdesign@gmail.com>
2019-05-07 22:43:23 -0400

    queries completed

--- querying/select.sql
+++ querying/select.sql
@@ -319,3 +319,265 @@ WHERE
   AND date_of_birth BETWEEN '1950-01-01'
   AND '1980-12-31';
 
+-- Ordering the resutls returned
+
+/*
+SELECT columnname1, columnname2 FROM tablename
+ORDER BY columnname3;
+ */
+-- Ascending by default
+
+SELECT
+  first_name,
+  last_name,
+  date_of_birth
+FROM
+  actors
+ORDER BY
+  first_name;
+
+-- Descending
+
+SELECT
+  first_name,
+  last_name,
+  date_of_birth
+FROM
+  actors
+ORDER BY
+  first_name DESC;
+
+-- Descending
+
+SELECT
+  first_name,
+  last_name,
+  date_of_birth
+FROM
+  actors
+ORDER BY
+  first_name ASC;
+
+-- descending integers
+
+SELECT
+  actor_id,
+  first_name,
+  last_name,
+  date_of_birth
+FROM
+  actors
+ORDER BY
+  actor_id DESC;
+
+SELECT
+  actor_id,
+  first_name,
+  last_name,
+  date_of_birth
+FROM
+  actors
+WHERE
+  gender = 'F'
+ORDER BY
+  date_of_birth DESC;
+
+-- Limiting the number of records returned
+
+/*
+SELECT columname1, columnname2 FROM tablename
+LIMIT N;
+ */
+SELECT
+  *
+FROM
+  movie_revenues
+ORDER BY
+  domestic_takings
+LIMIT 3;
+
+-- offset to skip first 3 rows
+
+SELECT
+  *
+FROM
+  movie_revenues
+ORDER BY
+  revenue_id
+LIMIT 5 OFFSET 3;
+
+-- Distinct values
+
+/*
+SELECT DISTINCT columnname FROM tablename;
+ */
+SELECT DISTINCT
+  movie_lang
+FROM
+  movies
+ORDER BY
+  movie_lang;
+
+-- distinct combinations
+
+SELECT DISTINCT
+  movie_lang,
+  age_certificate
+FROM
+  movies
+ORDER BY
+  movie_lang;
+
+-- challenge
+
+SELECT
+  *
+FROM
+  directors
+WHERE
+  nationality = 'American'
+ORDER BY
+  date_of_birth;
+
+SELECT DISTINCT
+  nationality
+FROM
+  directors;
+
+SELECT
+  first_name,
+  last_name,
+  date_of_birth
+FROM
+  actors
+WHERE
+  gender = 'F'
+ORDER BY
+  date_of_birth DESC
+LIMIT 10;
+
+-- Dealing with NULL values
+
+/*
+SELECT * FROM tablename
+WHERE columnanme IS NULL;
+
+SELECT * FROM tablename
+WHERE columnanme IS NOT NULL;
+ */
+SELECT
+  *
+FROM
+  ACTORS
+WHERE
+  date_of_birth IS NULL;
+
+SELECT
+  *
+FROM
+  ACTORS
+WHERE
+  date_of_birth IS NOT NULL;
+
+SELECT
+  *
+FROM
+  movie_revenues
+WHERE
+  domestic_takings IS NOT NULL
+ORDER BY
+  domestic_takings DESC;
+
+SELECT
+  *
+FROM
+  movie_revenues
+WHERE
+  international_takings IS NULL;
+
+-- Setting a column name alias
+
+/*
+SELECT columnname AS new_columnname FROM tablename;
+ */
+SELECT
+  last_name AS surname
+FROM
+  directors
+  SELECT
+    last_name AS surname
+  FROM
+    directors
+  WHERE
+    last_name = 'Anderson';
+
+SELECT
+  last_name AS surname
+FROM
+  directors
+WHERE
+  last_name LIKE 'A%'
+ORDER BY
+  surname;
+
+-- or without alias
+
+SELECT
+  last_name AS surname
+FROM
+  directors
+WHERE
+  last_name LIKE 'A%'
+ORDER BY
+  last_name;
+
+-- Using concatenate
+
+/*
+SELECT 'string1' || 'string2' AS new_string;
+SELECT CONCAT(column1, column2) AS new_column FROM tablename;
+SELECT CONCAT_WS(' ', column1, column2) AS new_column FROM tablename;
+ */
+SELECT
+  'concat' || 'together' AS new_string;
+
+SELECT
+  'concat' || ' ' || 'together' AS new_string;
+
+SELECT
+  CONCAT(first_name, ' ', last_name) AS full_name
+FROM
+  actors;
+
+-- WS = with separator
+
+SELECT
+  CONCAT_WS(' ', first_name, last_name, date_of_birth) AS full_name
+FROM
+  actors;
+
+-- challenge
+
+SELECT
+  *
+FROM
+  movie_revenues
+WHERE
+  international_takings IS NOT NULL
+ORDER BY
+  international_takings DESC
+LIMIT 3;
+
+SELECT
+  CONCAT_WS(' ', first_name, last_name) AS full_name
+FROM
+  directors;
+
+SELECT
+  *
+FROM
+  actors
+WHERE
+  first_name IS NULL
+  OR date_of_birth IS NULL;
+
